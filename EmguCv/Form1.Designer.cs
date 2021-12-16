7e28305da97acdbc0649c9ea65045eeb44deba39
Waldri Oliveira <waldrioliveira@pt.controlar.com>
2021-12-16 17:55:52 +0000

    Add project files.

---
+++ EmguCv/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace EmguCv
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOrign = new System.Windows.Forms.PictureBox();
+            this.picResize = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGray = new System.Windows.Forms.Button();
+            this.btnCam = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOrign)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOrign
+            // 
+            this.picOrign.Location = new System.Drawing.Point(35, 40);
+            this.picOrign.Name = "picOrign";
+            this.picOrign.Size = new System.Drawing.Size(291, 249);
+            this.picOrign.TabIndex = 0;
+            this.picOrign.TabStop = false;
+            // 
+            // picResize
+            // 
+            this.picResize.Location = new System.Drawing.Point(446, 40);
+            this.picResize.Name = "picResize";
+            this.picResize.Size = new System.Drawing.Size(291, 249);
+            this.picResize.TabIndex = 1;
+            this.picResize.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(142, 319);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGray
+            // 
+            this.btnGray.Location = new System.Drawing.Point(561, 319);
+            this.btnGray.Name = "btnGray";
+            this.btnGray.Size = new System.Drawing.Size(75, 23);
+            this.btnGray.TabIndex = 3;
+            this.btnGray.Text = "Gray";
+            this.btnGray.UseVisualStyleBackColor = true;
+            this.btnGray.Click += new System.EventHandler(this.btnGray_Click);
+            // 
+            // btnCam
+            // 
+            this.btnCam.Location = new System.Drawing.Point(346, 319);
+            this.btnCam.Name = "btnCam";
+            this.btnCam.Size = new System.Drawing.Size(75, 23);
+            this.btnCam.TabIndex = 4;
+            this.btnCam.Text = "Cemera";
+            this.btnCam.UseVisualStyleBackColor = true;
+            this.btnCam.Click += new System.EventHandler(this.btnCam_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(142, 357);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen2.TabIndex = 5;
+            this.btnOpen2.Text = "Open2";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.btnCam);
+            this.Controls.Add(this.btnGray);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picResize);
+            this.Controls.Add(this.picOrign);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picOrign)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOrign;
+        private System.Windows.Forms.PictureBox picResize;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGray;
+        private System.Windows.Forms.Button btnCam;
+        private System.Windows.Forms.Button btnOpen2;
+    }
+}
+
---
+++ EmguCv/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace EmguCv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpFile = new OpenFileDialog();
+            OpFile.Filter = "Image File (*.bmp, *.png) | *.bmp;*.png";
+            if(DialogResult.OK == OpFile.ShowDialog())
+            {
+                this.picOrign.Image = new Bitmap(OpFile.FileName);
+            }
+        }
+
+        private void btnCam_Click(object sender, EventArgs e)
+        {
+            ImageViewer viewer = new ImageViewer(); //create an image viewer
+            VideoCapture capture = new VideoCapture(); //create a camera captue
+            Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+            {  //run this until application closed (close button click on image viewer)
+                viewer.Image = capture.QueryFrame(); //draw the image obtained from camera
+            });
+            viewer.ShowDialog(); //show the image viewer
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            Bitmap ggg = new Bitmap((Bitmap)this.picOrign.Image);
+            Method.ConvertToGray(ggg);
+            this.picResize.Image = ggg;
+            
+        }
+
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            Mat img;
+            img = CvInvoke.Imread("images/flower.jpg");
+            CvInvoke.NamedWindow("Image", Emgu.CV.CvEnum.WindowFlags.Normal);
+            CvInvoke.Imshow("Image", img);
+            CvInvoke.Imwrite("images/flower.jpg", img);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
